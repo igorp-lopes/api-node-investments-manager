18471cfe88ea930b3ba471c24b4afb9b06c32079
igorplopes <igorp.lopes16@gmail.com>
2022-02-05 15:48:07 -0300

    feat: added validation for add stocks record endpoint
    added validation pipe globally

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './core/prisma.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 
--- src/stocks/stocks.controller.ts
+++ src/stocks/stocks.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { StocksService } from './stocks.service';
+import { Stocks } from '@prisma/client';
+import { RegisterStockDto } from './stocks.models';
 
 @Controller('stocks')
 export class StocksController {
   constructor(private stocksService: StocksService) {}
 
   @Post()
-  addStockRecords(): string {
-    return this.stocksService.addStockRecord();
+  async addStockRecord(@Body() stockData: RegisterStockDto): Promise<Stocks> {
+    return this.stocksService.addStockRecord(stockData);
   }
 }
