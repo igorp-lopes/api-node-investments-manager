162a665c7d39312f4fa2eb629c31e5e1a884152e
igorplopes <igorp.lopes16@gmail.com>
2022-02-04 23:03:52 -0300

    feat: added stocks module

--- src/stocks/stocks.repository.ts
+++ src/stocks/stocks.repository.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../core/prisma.service';
 
 @Injectable()
 export class StocksRepository {
+  constructor(private prisma: PrismaService) {}
+
   saveStock(): string {
     return 'This endpoint adds an stock record information for a given day';
   }
