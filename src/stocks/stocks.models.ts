5d6a1a1a2d13e885ad7234ef83920bacb48720a0
igorplopes <igorp.lopes16@gmail.com>
2022-02-22 23:12:19 -0300

    feat: added mapping to isolate stock records DTOs from the stock record entity

--- src/stocks/stocks.models.ts
+++ src/stocks/stocks.models.ts
@@ -7,6 +7,22 @@ import {
 } from 'class-validator';
 import { Type } from 'class-transformer';
 
+export interface StocksRegisterEntity {
+  stock: string;
+  day: string;
+  contribution: number;
+  quotas: number;
+  currentQuotaValue: number;
+  currentValue: number;
+  meanQuotaValue: number;
+  investedValue: number;
+  dailyVariation: number;
+  dailyVariationPercent: number;
+  variation: number;
+  variationPercent: number;
+  category: string;
+}
+
 export class RegisterStockRequestDto {
   @IsString()
   readonly stock: string;
@@ -57,4 +73,35 @@ export interface StocksRegisterResponseDto {
   variation: number;
   variation_percent: number;
   category: string;
+}
+
+export function mapStockRegisterRequestToModel(
+  request: RegisterStockRequestDto,
+) {
+  return {
+    stock: request,
+    day: request.day,
+    contribution: request.contribution,
+    quotas: request.quotas,
+    currentQuotaValue: request.current_quota_value,
+    category: request.category,
+  };
+}
+
+export function mapStockModelToResponseDto(record: StocksRegisterEntity) {
+  return {
+    stock: record.stock,
+    day: record.day,
+    contribution: record.contribution,
+    quotas: record.quotas,
+    current_quota_value: record.currentQuotaValue,
+    current_value: record.currentValue,
+    mean_quota_value: record.meanQuotaValue,
+    invested_value: record.investedValue,
+    daily_variation: record.dailyVariation,
+    daily_variation_percent: record.dailyVariationPercent,
+    variation: record.variation,
+    variation_percent: record.variationPercent,
+    category: record.category,
+  };
 }
