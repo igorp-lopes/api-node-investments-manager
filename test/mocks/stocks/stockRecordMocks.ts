cb0644c976e9970193ae9e39f460dc7a727c15e4
igorplopes <igorp.lopes16@gmail.com>
2022-03-01 14:04:06 -0300

    feat: add simple stock record mock

---
+++ test/mocks/stocks/stockRecordMocks.ts
@@ -0,0 +1,19 @@
+const currentTime = new Date(Date.now());
+
+export const testStockRecord1 = {
+  stock: 'testStock1',
+  category: 'stocks',
+  day: '2022-02-18',
+  quotas: 55,
+  currentQuotaValue: 130,
+  meanQuotaValue: 130,
+  contribution: 7150,
+  currentValue: 130,
+  investedValue: 130,
+  dailyVariation: 0,
+  dailyVariationPercent: 0,
+  variation: 0,
+  variationPercent: 0,
+  createdAt: currentTime.toISOString(),
+  updatedAt: currentTime.toISOString(),
+};
