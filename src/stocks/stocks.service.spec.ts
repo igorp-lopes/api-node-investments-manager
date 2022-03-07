4ee472ef810d60f3bd4b117b5c404d576c976b74
igorplopes <igorp.lopes16@gmail.com>
2022-03-06 22:42:48 -0300

    feat: add stocks service unit tests

--- src/stocks/stocks.service.spec.ts
+++ src/stocks/stocks.service.spec.ts
@@ -2,19 +2,56 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { StocksService } from './stocks.service';
 import { StocksRepository } from './stocks.repository';
 import { PrismaService } from '../core/prisma.service';
+import {
+  testRegisterStockRequest,
+  testStockRecord1,
+} from '../../test/mocks/stockMocks';
+import { ErrorsService } from '../core/errors/errors.service';
+
+const db = {
+  stocks: {
+    findFirst: jest.fn().mockResolvedValue(null),
+    create: jest.fn().mockReturnValue(null),
+    delete: jest.fn().mockResolvedValue(null),
+  },
+};
 
 describe('Stocks Service Unit Tests', () => {
   let service: StocksService;
+  let repository: StocksRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StocksService, StocksRepository, PrismaService],
+      providers: [
+        StocksService,
+        StocksRepository,
+        ErrorsService,
+        {
+          provide: PrismaService,
+          useValue: db,
+        },
+      ],
     }).compile();
 
     service = module.get<StocksService>(StocksService);
+    repository = module.get<StocksRepository>(StocksRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('addStockRecord', () => {
+    describe('when adding a record that already exists', () => {
+      it('should return a error when there is already record on the same date', async () => {
+        const stocksRepositorySpy = jest
+          .spyOn(repository, 'getPreviousStockRecordsFromDate')
+          .mockResolvedValueOnce(testStockRecord1);
+
+        await expect(
+          service.addStockRecord(testRegisterStockRequest),
+        ).rejects.toThrowError();
+      });
+    });
   });
 });
