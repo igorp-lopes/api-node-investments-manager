2b4ec69561d75f99f8a20cbf143373c448866922
igorplopes <igorp.lopes16@gmail.com>
2022-03-19 00:00:17 -0300

    feat: added unit test to validate creating a stock record when previous records exist

--- src/stocks/stocks.service.spec.ts
+++ src/stocks/stocks.service.spec.ts
@@ -3,10 +3,13 @@ import { StocksService } from './stocks.service';
 import { StocksRepository } from './stocks.repository';
 import { PrismaService } from '../core/prisma.service';
 import {
-  testRegisterStockRequest,
+  testRegisterStockRequest1,
+  testRegisterStockRequest2,
   testStockRecord1,
+  testStockRecord2,
 } from '../../test/mocks/stockMocks';
 import { ErrorsService } from '../core/errors/errors.service';
+import { StocksRegisterResponseDto } from './stocks.models';
 
 const db = {
   stocks: {
@@ -14,6 +17,10 @@ const db = {
     create: jest.fn().mockReturnValue(null),
     delete: jest.fn().mockResolvedValue(null),
   },
+};
+
+const convertDateToProperString = (date) => {
+  return date.toISOString().split('T')[0];
 };
 
 describe('Stocks Service Unit Tests', () => {
@@ -42,15 +49,110 @@ describe('Stocks Service Unit Tests', () => {
   });
 
   describe('addStockRecord', () => {
+    const basicStockRecordValidation = (
+      stockRecord: StocksRegisterResponseDto,
+      registerStockRequest,
+    ) => {
+      expect(stockRecord.stock).toEqual(
+        registerStockRequest.stock.toUpperCase(),
+      );
+      expect(stockRecord.day).toEqual(
+        convertDateToProperString(registerStockRequest.day),
+      );
+      expect(stockRecord.quotas).toEqual(registerStockRequest.quotas);
+      expect(stockRecord.current_quota_value).toEqual(
+        registerStockRequest.current_quota_value,
+      );
+      expect(stockRecord.contribution).toEqual(
+        registerStockRequest.contribution,
+      );
+      expect(stockRecord.category).toEqual(registerStockRequest.category);
+    };
+
     describe('when adding a record that already exists', () => {
       it('should return a error when there is already record on the same date', async () => {
-        const stocksRepositorySpy = jest
-          .spyOn(repository, 'getPreviousStockRecordsFromDate')
+        jest
+          .spyOn(repository, 'getStockRecordByNameAndDate')
           .mockResolvedValueOnce(testStockRecord1);
 
         await expect(
-          service.addStockRecord(testRegisterStockRequest),
+          service.addStockRecord(testRegisterStockRequest1),
         ).rejects.toThrowError();
+      });
+    });
+
+    describe('when adding a record with no previous record', () => {
+      it('should save a record with no variations', async () => {
+        jest
+          .spyOn(repository, 'getStockRecordByNameAndDate')
+          .mockResolvedValueOnce(null);
+
+        jest
+          .spyOn(repository, 'getPreviousStockRecordsFromDate')
+          .mockResolvedValueOnce(null);
+
+        jest
+          .spyOn(repository, 'saveStock')
+          .mockResolvedValueOnce(testStockRecord1);
+
+        const addStockRecordResponse = await service.addStockRecord(
+          testRegisterStockRequest1,
+        );
+
+        basicStockRecordValidation(
+          addStockRecordResponse,
+          testRegisterStockRequest1,
+        );
+
+        expect(addStockRecordResponse.variation).toBeCloseTo(0);
+        expect(addStockRecordResponse.variation_percent).toBeCloseTo(0);
+        expect(addStockRecordResponse.daily_variation).toBeCloseTo(0);
+        expect(addStockRecordResponse.daily_variation_percent).toBeCloseTo(0);
+
+        expect(addStockRecordResponse.mean_quota_value).toBeCloseTo(
+          testRegisterStockRequest1.current_quota_value,
+        );
+
+        expect(addStockRecordResponse.invested_value).toBeCloseTo(
+          testRegisterStockRequest1.quotas *
+            addStockRecordResponse.mean_quota_value,
+        );
+      });
+    });
+
+    describe('when adding a record with previous records', () => {
+      it('should save a record with correct info that is inferred based on previous records', async () => {
+        jest
+          .spyOn(repository, 'getStockRecordByNameAndDate')
+          .mockResolvedValueOnce(null);
+
+        jest
+          .spyOn(repository, 'getPreviousStockRecordsFromDate')
+          .mockResolvedValueOnce(testStockRecord1);
+
+        jest
+          .spyOn(repository, 'saveStock')
+          .mockResolvedValueOnce(testStockRecord2);
+
+        const addStockRecordResponse = await service.addStockRecord(
+          testRegisterStockRequest2,
+        );
+
+        basicStockRecordValidation(
+          addStockRecordResponse,
+          testRegisterStockRequest2,
+        );
+
+        expect(addStockRecordResponse.variation).toBeCloseTo(275);
+        expect(addStockRecordResponse.variation_percent).toBeCloseTo(0.035);
+        expect(addStockRecordResponse.daily_variation).toBeCloseTo(275);
+        expect(addStockRecordResponse.daily_variation_percent).toBeCloseTo(
+          0.038,
+        );
+
+        expect(addStockRecordResponse.mean_quota_value).toBeCloseTo(130.416);
+
+        expect(addStockRecordResponse.invested_value).toBeCloseTo(7824.999);
       });
     });
   });
--- test/mocks/stockMocks.ts
+++ test/mocks/stockMocks.ts
@@ -4,7 +4,7 @@ import {
   StocksRegisterResponseDto,
 } from '../../src/stocks/stocks.models';
 
-export const testRegisterStockRequest: RegisterStockRequestDto = {
+export const testRegisterStockRequest1: RegisterStockRequestDto = {
   stock: 'testStock1',
   day: new Date('2022-02-18'),
   quotas: 55,
@@ -15,25 +15,69 @@ export const testRegisterStockRequest: RegisterStockRequestDto = {
 
 export const testStockRecord1: Stocks = {
   id: 1,
-  stock: 'testStock1',
+  stock: 'TESTSTOCK1',
   category: 'international',
   day: new Date('2022-02-18'),
   quotas: 55,
   currentQuotaValue: 130,
   meanQuotaValue: 130,
   contribution: 7150,
-  currentValue: 130,
-  investedValue: 130,
+  currentValue: 7150,
+  investedValue: 7150,
   dailyVariation: 0,
   dailyVariationPercent: 0,
   variation: 0,
   variationPercent: 0,
-  createdAt: new Date('2022-03-17T00:47:19.292Z'),
-  updatedAt: new Date('2022-03-17T00:47:19.292Z'),
+  createdAt: new Date('2022-03-18T00:47:19.292Z'),
+  updatedAt: new Date('2022-03-18T00:47:19.292Z'),
 };
 
 export const testRegisterStockResponse1: StocksRegisterResponseDto = {
+  stock: 'TESTSTOCK1',
+  category: 'international',
+  day: '2022-02-18',
+  quotas: 55,
+  current_quota_value: 130,
+  mean_quota_value: 130,
+  contribution: 7150,
+  current_value: 7150,
+  invested_value: 7150,
+  daily_variation: 0,
+  daily_variation_percent: 0,
+  variation: 0,
+  variation_percent: 0,
+};
+
+export const testRegisterStockRequest2: RegisterStockRequestDto = {
   stock: 'testStock1',
+  day: new Date('2022-02-19'),
+  quotas: 60,
+  current_quota_value: 135,
+  contribution: 675,
+  category: 'international',
+};
+
+export const testStockRecord2: Stocks = {
+  id: 2,
+  stock: 'TESTSTOCK1',
+  category: 'international',
+  day: new Date('2022-02-19'),
+  quotas: 60,
+  currentQuotaValue: 135,
+  meanQuotaValue: 130,
+  contribution: 675,
+  currentValue: 8100,
+  investedValue: 7824.999999999999,
+  dailyVariation: 275,
+  dailyVariationPercent: 0.038461538461538464,
+  variation: 275.0000000000009,
+  variationPercent: 0.03514376996805124,
+  createdAt: new Date('2022-03-19T00:47:19.292Z'),
+  updatedAt: new Date('2022-03-19T00:47:19.292Z'),
+};
+
+export const testRegisterStockResponse2: StocksRegisterResponseDto = {
+  stock: 'TESTSTOCK1',
   category: 'international',
   day: '2022-02-18',
   quotas: 55,
