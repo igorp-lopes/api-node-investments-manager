56972fc53403cc52276a6e8330f0f12797065ea0
igorplopes <igorp.lopes16@gmail.com>
2022-04-02 15:52:41 -0300

    feat: added basic setup for stocks e2e tests

---
+++ test/stocks.e2e-spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { StocksModule } from '../src/stocks/stocks.module';
+import { PrismaService } from '../src/core/prisma.service';
+import { cleanupDatabase, setupGlobalModules } from './testUtils';
+
+describe('Stocks End-to-End Tests', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [StocksModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    setupGlobalModules(app);
+
+    prisma = app.get<PrismaService>(PrismaService);
+
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await cleanupDatabase(prisma);
+  });
+});
