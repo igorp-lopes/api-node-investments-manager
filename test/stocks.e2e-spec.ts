55fde33c7526a21f7356ab9d582d8617ddb0418f
igorplopes <igorp.lopes16@gmail.com>
2022-04-03 13:59:54 -0300

    test: add test for creating a stock record without previous records

--- test/stocks.e2e-spec.ts
+++ test/stocks.e2e-spec.ts
@@ -4,6 +4,7 @@ import * as request from 'supertest';
 import { StocksModule } from '../src/stocks/stocks.module';
 import { PrismaService } from '../src/core/prisma.service';
 import { cleanupDatabase, setupGlobalModules } from './testUtils';
+import { RegisterStockRequestDto } from '../src/stocks/stocks.models';
 
 describe('Stocks End-to-End Tests', () => {
   let app: INestApplication;
@@ -25,6 +26,35 @@ describe('Stocks End-to-End Tests', () => {
   afterAll(async () => {
     await cleanupDatabase(prisma);
   });
+
+  it('Creating a stock record without previous records', async () => {
+    const requestBody = createStockRecordDTO(
+      'stockWithNoRecord',
+      '2022-04-01',
+      10,
+      55.5,
+    );
+
+    const postResponse = await request(app.getHttpServer())
+      .post('/stocks')
+      .send(requestBody);
+    const content = postResponse.body;
+
+    expect(postResponse.status).toEqual(201);
+    expect(content.stock).toEqual(requestBody.stock.toUpperCase());
+    expect(content.quotas).toEqual(requestBody.quotas);
+    expect(content.day).toEqual(requestBody.day);
+
+    expect(content.mean_quota_value).toBeCloseTo(55.5);
+    expect(content.contribution).toBeCloseTo(555);
+    expect(content.invested_value).toBeCloseTo(555);
+
+    expect(content.daily_variation).toBeCloseTo(0);
+    expect(content.daily_variation_percent).toBeCloseTo(0);
+
+    expect(content.variation).toBeCloseTo(0);
+    expect(content.variation_percent).toBeCloseTo(0);
+  });
 });
 
 const createStockRecordDTO = (
@@ -40,5 +70,5 @@ const createStockRecordDTO = (
     quotas,
     current_quota_value: currentQuotaValue,
     category: category ?? 'stocks',
-  };
+  } as RegisterStockRequestDto;
 };
