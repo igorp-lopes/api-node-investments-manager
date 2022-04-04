e68462ccc27ff9cd4d52979f367810f1741178d7
igorplopes <igorp.lopes16@gmail.com>
2022-04-03 22:48:53 -0300

    test: added seed data for repeated stock record test

--- test/seeds/stocksSeeds.ts
+++ test/seeds/stocksSeeds.ts
@@ -19,9 +19,27 @@ export const seedStockWithPreviousRecord: Prisma.StocksCreateInput = {
   updatedAt: '2022-04-03T17:33:47.202Z',
 };
 
+export const seedStockWithRepeatedRecord: Prisma.StocksCreateInput = {
+  stock: 'STOCKWITHREPEATEDRECORD',
+  day: '2022-04-01T00:00:00.000Z',
+  contribution: 555,
+  quotas: 10,
+  currentQuotaValue: 55.5,
+  currentValue: 555,
+  meanQuotaValue: 55.5,
+  investedValue: 555,
+  dailyVariation: 0,
+  dailyVariationPercent: 0,
+  variation: 0,
+  variationPercent: 0,
+  category: 'stocks',
+  createdAt: '2022-04-03T17:33:47.202Z',
+  updatedAt: '2022-04-03T17:33:47.202Z',
+};
+
 export const setupStocksDatabase = async (prisma: PrismaService) => {
   await prisma.stocks.createMany({
-    data: [seedStockWithPreviousRecord],
+    data: [seedStockWithPreviousRecord, seedStockWithRepeatedRecord],
   });
 
   await prisma.$disconnect();
