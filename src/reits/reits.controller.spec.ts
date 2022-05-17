510e6c3581a9ed25a60d1b7edcb555308b8812c7
igorplopes <igorp.lopes16@gmail.com>
2022-05-16 21:51:40 -0300

    fix: correct unit tests for reits controller, service and app module

--- src/reits/reits.controller.spec.ts
+++ src/reits/reits.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReitsController } from './reits.controller';
+import { PrismaService } from '../core/prisma.service';
+import { ReitsService } from './reits.service';
+import { ReitsRepository } from './reits.repository';
 
 describe('ReitsController', () => {
   let controller: ReitsController;
@@ -7,6 +10,7 @@ describe('ReitsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ReitsController],
+      providers: [ReitsService, ReitsRepository, PrismaService],
     }).compile();
 
     controller = module.get<ReitsController>(ReitsController);
--- src/reits/reits.service.spec.ts
+++ src/reits/reits.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReitsService } from './reits.service';
+import { ReitsRepository } from './reits.repository';
+import { PrismaService } from '../core/prisma.service';
 
 describe('ReitsService', () => {
   let service: ReitsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReitsService],
+      providers: [ReitsService, ReitsRepository, PrismaService],
     }).compile();
 
     service = module.get<ReitsService>(ReitsService);
