68c892eb4b392193e16ad4a96c8daae6d1f2894e
igorplopes <igorp.lopes16@gmail.com>
2022-05-16 21:09:14 -0300

    feat: add simple test method for reits service
    update reits test endpoint to use test reits service method

--- src/reits/reits.controller.ts
+++ src/reits/reits.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ReitsService } from './reits.service';
 
 @Controller('reits')
 export class ReitsController {
+  constructor(private reitsService: ReitsService) {}
+
   @Get('/:test_value')
   async testReitEndpoint(@Param() params): Promise<any> {
     const value = params.test_value;
-    return value;
+    return this.reitsService.testReitServiceMethod(value);
   }
 }
