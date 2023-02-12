f07d2028c81cb8d08b1ec90525fe4a5821cab34c
igorplopes <igorp.lopes16@gmail.com>
2023-02-12 11:35:09 -0300

    feat: implement custom logger configs
    add file logging for general info and errors
    
    select transports and logging level according to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,12 @@ import { ErrorsModule } from './core/errors/errors.module';
 import { APP_FILTER } from '@nestjs/core';
 import { CustomHttpExceptionFilter } from './core/errors/http-custom-exception.filter';
 import { ReitsModule } from './reits/reits.module';
+import { pinoLoggerConfigs } from './core/logger/logger.config';
 import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
-    LoggerModule.forRoot({
-      pinoHttp: { transport: { target: 'pino-pretty' } },
-    }),
+    LoggerModule.forRoot(pinoLoggerConfigs),
     StocksModule,
     ErrorsModule,
     ReitsModule,
