bebe9c7f8a616413bd2e0794e405371bd503b1b5
igorplopes <igorp.lopes16@gmail.com>
2023-04-08 00:06:22 -0300

    test: update e2e tests to use arbitrally generated stock records

--- test/mocks/stocksBuilders.ts
+++ test/mocks/stocksBuilders.ts
@@ -1,185 +1,131 @@
 import { RegisterStockRequest } from '../../src/stocks/stocks.schemas';
 import { faker } from '@faker-js/faker';
-import { Prisma } from '@prisma/client';
-import { StockClientEntity } from '../../src/stocks/stocks.models';
-import { generateFakerDate } from '../utils/generalTestUtils';
+import { Stock } from '../../src/stocks/stocks.models';
+import { addDaysToDate, generateFakerDate } from '../utils/generalTestUtils';
+import { v4 as uuidv4 } from 'uuid';
+import { calculateMeanQuotaValue } from '../../src/helpers/financeHelpers';
 
-export class StocksRequestBuilder {
-  createStockRecordRequest(): RegisterStockRequest {
-    return this.buildStockRecordRequest();
+const NUMBER_MIN_MAX_LIMITS = { min: 1, max: 1000 };
+const FLOAT_MIN_MAX_LIMITS = { min: 1, max: 1000, precision: 0.01 };
+export class StockRecordsBuilder {
+  public createStockRecordWithoutPreviousRecord(): Stock {
+    const quotas = faker.datatype.number(NUMBER_MIN_MAX_LIMITS);
+    const currentQuotaValue = faker.datatype.float(FLOAT_MIN_MAX_LIMITS);
+    const contribution = quotas * currentQuotaValue;
+
+    return this.buildStockRecord({
+      quotas,
+      currentQuotaValue,
+      contribution: parseFloat(contribution.toFixed(2)),
+      meanQuotaValue: currentQuotaValue,
+      dailyVariation: 0,
+      dailyVariationPercent: 0,
+    });
   }
 
-  createStockRecordRequestFromPreviousRecordRequest(
-    previousRequest: Partial<RegisterStockRequest>,
-  ): RegisterStockRequest {
-    const date = generateFakerDate(faker.date.soon(2, previousRequest.date));
+  public createStockRecordFromPreviousRecord(previousRecord: Stock): Stock {
+    const date = generateFakerDate(addDaysToDate(previousRecord.date));
     const quotas = faker.datatype.number({
-      min: previousRequest.quotas + 1,
-      max: previousRequest.quotas * 3,
+      min: previousRecord.quotas + 1,
+      max: previousRecord.quotas * 3,
     });
     const currentQuotaValue = faker.datatype.float({
-      min: previousRequest.current_quota_value + 1,
-      max: previousRequest.current_quota_value * 3,
+      min: previousRecord.currentQuotaValue + 1,
+      max: previousRecord.currentQuotaValue * 3,
+      precision: 0.01,
     });
 
-    return this.buildStockRecordRequest({
-      stock: previousRequest.stock,
+    const quotaDiff = quotas - previousRecord.quotas;
+    const contribution = quotaDiff * currentQuotaValue;
+
+    const meanQuotaValue = calculateMeanQuotaValue(
+      previousRecord.meanQuotaValue,
+      previousRecord.quotas,
+      currentQuotaValue,
+      quotaDiff,
+    );
+
+    const dailyVariation =
+      quotas * currentQuotaValue - previousRecord.currentValue - contribution;
+
+    const dailyVariationPercent = dailyVariation / previousRecord.currentValue;
+
+    return this.buildStockRecord({
+      stock: previousRecord.stock,
       date,
       quotas,
-      current_quota_value: currentQuotaValue,
-      category: previousRequest.category,
+      currentQuotaValue,
+      contribution: parseFloat(contribution.toFixed(2)),
+      meanQuotaValue: parseFloat(meanQuotaValue.toFixed(2)),
+      dailyVariation: parseFloat(dailyVariation.toFixed(2)),
+      dailyVariationPercent: parseFloat(dailyVariationPercent.toFixed(2)),
+    });
+  }
+  private buildStockRecord(stocksParams?: Partial<Stock>): Stock {
+    const quotas =
+      stocksParams.quotas ?? faker.datatype.number(NUMBER_MIN_MAX_LIMITS);
+
+    const meanQuotaValue =
+      stocksParams.meanQuotaValue ?? faker.datatype.float(FLOAT_MIN_MAX_LIMITS);
+
+    const investedValue = meanQuotaValue * quotas;
+
+    const currentQuotaValue =
+      stocksParams.currentQuotaValue ??
+      faker.datatype.float(FLOAT_MIN_MAX_LIMITS);
+
+    const currentValue = currentQuotaValue * quotas;
+
+    const variation = currentValue - investedValue;
+    const variationPercent = currentValue / investedValue - 1;
+
+    const stock = stocksParams.stock ?? faker.helpers.unique(faker.word.noun);
+
+    return {
+      stock: stock.toUpperCase(),
+      date: stocksParams.date ?? generateFakerDate(faker.date.soon()),
+      contribution:
+        stocksParams.contribution ??
+        faker.datatype.float(NUMBER_MIN_MAX_LIMITS),
+      category: stocksParams.category ?? faker.word.noun(),
+      dailyVariation: stocksParams.dailyVariation ?? 0,
+      dailyVariationPercent: stocksParams.dailyVariationPercent ?? 0,
+      variation: parseFloat(variation.toFixed(2)),
+      variationPercent: parseFloat(variationPercent.toFixed(2)),
+      quotas,
+      currentQuotaValue: parseFloat(currentQuotaValue.toFixed(2)),
+      currentValue: parseFloat(currentValue.toFixed(2)),
+      meanQuotaValue: parseFloat(meanQuotaValue.toFixed(2)),
+      investedValue: parseFloat(investedValue.toFixed(2)),
+      id: uuidv4(),
+    };
+  }
+}
+
+export class StocksRequestBuilder {
+  public createStockRecordRequestFromStockEntity(
+    entity: Stock,
+  ): RegisterStockRequest {
+    return this.buildStockRecordRequest({
+      stock: entity.stock,
+      category: entity.category,
+      date: entity.date,
+      quotas: entity.quotas,
+      current_quota_value: entity.currentQuotaValue,
     });
   }
   private buildStockRecordRequest(
     requestParams?: Partial<RegisterStockRequest>,
   ): RegisterStockRequest {
-    const numberMinMaxLimits = { min: 1, max: 1000 };
-
     return {
       stock: requestParams?.stock ?? faker.word.noun(),
       date: requestParams?.date ?? generateFakerDate(faker.date.soon()),
       quotas:
-        requestParams?.quotas ?? faker.datatype.number(numberMinMaxLimits),
+        requestParams?.quotas ?? faker.datatype.number(NUMBER_MIN_MAX_LIMITS),
       current_quota_value:
         requestParams?.current_quota_value ??
-        faker.datatype.float(numberMinMaxLimits),
+        faker.datatype.float(NUMBER_MIN_MAX_LIMITS),
       category: requestParams?.category ?? faker.word.noun(),
-    };
-  }
-}
-
-export class StocksResponseBuilder {
-  createStockRecordWithoutPreviousRecordResponseFromRequest(
-    request: RegisterStockRequest,
-  ): StockClientEntity {
-    return this.buildStockRecordResponse({
-      stock: request.stock,
-      date: request.date.toISOString().split('T')[0],
-      quotas: request.quotas,
-      current_quota_value: request.current_quota_value,
-      mean_quota_value: request.current_quota_value,
-      category: request.category,
-      daily_variation: 0,
-      daily_variation_percent: 0,
-      contribution: request.quotas * request.current_quota_value,
-    });
-  }
-
-  createStockRecordResponseFromRequest(
-    request: RegisterStockRequest,
-  ): StockClientEntity {
-    return this.buildStockRecordResponse({
-      stock: request.stock,
-      date: request.date.toISOString().split('T')[0],
-      quotas: request.quotas,
-      current_quota_value: request.current_quota_value,
-      category: request.category,
-    });
-  }
-
-  private buildStockRecordResponse(
-    responseParams?: Partial<StockClientEntity>,
-  ): StockClientEntity {
-    const numberMinMaxLimits = { min: 1, max: 1000 };
-    const date =
-      responseParams.date ??
-      generateFakerDate(faker.date.soon()).toISOString().split('T')[0];
-
-    const quotas =
-      responseParams.quotas ?? faker.datatype.number(numberMinMaxLimits);
-
-    const meanQuotaValue =
-      responseParams.mean_quota_value ??
-      faker.datatype.float(numberMinMaxLimits);
-
-    const investedValue = meanQuotaValue * quotas;
-
-    const currentQuotaValue =
-      responseParams.current_quota_value ??
-      faker.datatype.float(numberMinMaxLimits);
-
-    const currentValue = currentQuotaValue * quotas;
-
-    const stock = responseParams.stock ?? faker.word.noun();
-
-    return {
-      date,
-      quotas,
-      current_quota_value: currentQuotaValue,
-      current_value: currentValue,
-      mean_quota_value: meanQuotaValue,
-      invested_value: investedValue,
-      variation: currentValue - investedValue,
-      variation_percent: currentValue / investedValue - 1,
-      stock: stock.toUpperCase(),
-      contribution:
-        responseParams.contribution ?? faker.datatype.float(numberMinMaxLimits),
-      daily_variation:
-        responseParams.daily_variation ??
-        faker.datatype.float(numberMinMaxLimits),
-      daily_variation_percent:
-        responseParams.daily_variation_percent ??
-        faker.datatype.float({ min: 0, max: 2 }),
-      category: responseParams.category,
-    };
-  }
-}
-
-export class StocksDbBuilder {
-  createDbStockWithoutPreviousRecordFromStockRequest(
-    stockRequest: Partial<RegisterStockRequest>,
-  ): Prisma.StocksCreateInput {
-    const {
-      stock,
-      date,
-      quotas,
-      current_quota_value: currentQuotaValue,
-      category,
-    } = stockRequest;
-
-    return this.buildDbStock({
-      stock,
-      date,
-      quotas,
-      currentQuotaValue,
-      meanQuotaValue: currentQuotaValue,
-      category,
-    });
-  }
-  private buildDbStock(
-    dbStockParams?: Partial<Prisma.StocksCreateInput>,
-  ): Prisma.StocksCreateInput {
-    const floatMinMaxLimits = { min: 1, max: 1000 };
-
-    const currentDate = new Date();
-    const quotas =
-      dbStockParams.quotas ?? faker.datatype.float(floatMinMaxLimits);
-    const currentQuotaValue =
-      dbStockParams.currentQuotaValue ??
-      faker.datatype.float(floatMinMaxLimits);
-    const currentValue = currentQuotaValue * quotas;
-    const meanQuotaValue =
-      dbStockParams.meanQuotaValue ?? faker.datatype.float(floatMinMaxLimits);
-    const investedValue = meanQuotaValue * quotas;
-
-    const stock = dbStockParams.stock ?? faker.word.noun();
-
-    return {
-      stock: stock.toUpperCase(),
-      date: dbStockParams.date ?? generateFakerDate(faker.date.soon()),
-      createdAt: dbStockParams.createdAt ?? currentDate,
-      updatedAt: dbStockParams.updatedAt ?? currentDate,
-      currentQuotaValue,
-      quotas,
-      currentValue,
-      contribution: dbStockParams.contribution ?? currentValue,
-      meanQuotaValue,
-      investedValue,
-      dailyVariation: dbStockParams.dailyVariation ?? 0,
-      dailyVariationPercent: dbStockParams.dailyVariationPercent ?? 0,
-      variation: currentValue - investedValue,
-      variationPercent: currentValue / investedValue - 1,
-      category: dbStockParams.category ?? faker.word.noun(),
     };
   }
 }
--- test/stocks.e2e-spec.ts
+++ test/stocks.e2e-spec.ts
@@ -4,20 +4,16 @@ import * as request from 'supertest';
 import { StocksModule } from '../src/stocks/stocks.module';
 import { PrismaService } from '../src/core/prisma.service';
 import { cleanupDatabase, setupGlobalModules } from './utils/generalTestUtils';
-import {
-  testStockWithoutRepeatedRecordRequest2,
-  testStockWithoutRepeatedRecordResponse2,
-} from './mocks/stockMocks';
 import { addManyStockRecords, setupStocksDatabase } from './seeds/stocksSeeds';
 import {
-  StocksDbBuilder,
+  StockRecordsBuilder,
   StocksRequestBuilder,
-  StocksResponseBuilder,
 } from './mocks/stocksBuilders';
+import { StocksMapper } from '../src/stocks/stocks.mapper';
 
+const stockRecordsBuilder = new StockRecordsBuilder();
 const stocksRequestBuilder = new StocksRequestBuilder();
-const stocksResponseBuilder = new StocksResponseBuilder();
-const stocksDbBuilder = new StocksDbBuilder();
+const stocksMapper = new StocksMapper();
 
 describe('Stocks End-to-End Tests', () => {
   let app: INestApplication;
@@ -43,21 +39,16 @@ describe('Stocks End-to-End Tests', () => {
     await cleanupDatabase(prisma);
   });
 
-  const createStockRecordCases = [
-    [
-      'without previous records',
-      testStockWithoutRepeatedRecordRequest2,
-      testStockWithoutRepeatedRecordResponse2,
-    ],
-    // [
-    //   'with previous records',
-    //   testStockWithPreviousRecordRequest,
-    //   testStockWithPreviousRecordResponse,
-    // ],
-  ];
-  test.each(createStockRecordCases)(
-    'Creating a stock record %s',
-    async (testName, requestBody, expectedResponse) => {
+  describe('(POST) /stocks', () => {
+    test('Creating a stock record without previous records', async () => {
+      const stockEntity =
+        stockRecordsBuilder.createStockRecordWithoutPreviousRecord();
+      const requestBody =
+        stocksRequestBuilder.createStockRecordRequestFromStockEntity(
+          stockEntity,
+        );
+      const expectedResponse = stocksMapper.fromEntityToClient(stockEntity);
+
       const postResponse = await request(app.getHttpServer())
         .post('/stocks')
         .send(requestBody);
@@ -65,63 +56,97 @@ describe('Stocks End-to-End Tests', () => {
 
       expect(postResponse.status).toEqual(201);
       expect(content).toMatchObject(expectedResponse);
-    },
-  );
+    });
 
-  test('Creating a stock record that already exists', async () => {
-    const stockRequest = stocksRequestBuilder.createStockRecordRequest();
-    const dbStockRecord =
-      stocksDbBuilder.createDbStockWithoutPreviousRecordFromStockRequest(
-        stockRequest,
-      );
-    await addManyStockRecords(prisma, [dbStockRecord]);
+    test('Creating a stock record with previous records', async () => {
+      const previousStockEntity =
+        stockRecordsBuilder.createStockRecordWithoutPreviousRecord();
 
-    const postResponse = await request(app.getHttpServer())
-      .post('/stocks')
-      .send(stockRequest);
-    const content = postResponse.body;
+      const stockEntity =
+        stockRecordsBuilder.createStockRecordFromPreviousRecord(
+          previousStockEntity,
+        );
 
-    expect(postResponse.status).toEqual(400);
-    expect(content.name).toEqual('EST001');
+      const previousDbStockRecord =
+        stocksMapper.fromEntityToPersistence(previousStockEntity);
+      await addManyStockRecords(prisma, [previousDbStockRecord]);
+
+      const requestBody =
+        stocksRequestBuilder.createStockRecordRequestFromStockEntity(
+          stockEntity,
+        );
+      const expectedResponse = stocksMapper.fromEntityToClient(stockEntity);
+
+      const postResponse = await request(app.getHttpServer())
+        .post('/stocks')
+        .send(requestBody);
+      const content = postResponse.body;
+
+      expect(postResponse.status).toEqual(201);
+      expect(content).toMatchObject(expectedResponse);
+    });
+
+    test('Creating a stock record that already exists', async () => {
+      const stockEntity =
+        stockRecordsBuilder.createStockRecordWithoutPreviousRecord();
+
+      const stockRequest =
+        stocksRequestBuilder.createStockRecordRequestFromStockEntity(
+          stockEntity,
+        );
+
+      const dbStockRecord = stocksMapper.fromEntityToPersistence(stockEntity);
+      await addManyStockRecords(prisma, [dbStockRecord]);
+
+      const postResponse = await request(app.getHttpServer())
+        .post('/stocks')
+        .send(stockRequest);
+      const content = postResponse.body;
+
+      expect(postResponse.status).toEqual(400);
+      expect(content.name).toEqual('EST001');
+    });
   });
 
-  const deleteStockRecordCases = [
-    ['from a single date', '2022-04-01', '', 1],
-    ['from a date interval', '2022-04-02', '2022-04-03', 2],
-  ];
+  describe('(DELETE) /stocks/{stock_name}', () => {
+    const deleteStockRecordCases = [
+      ['from a single date', '2022-04-01', '', 1],
+      ['from a date interval', '2022-04-02', '2022-04-03', 2],
+    ];
 
-  test.each(deleteStockRecordCases)(
-    'Deleting stock records %s',
-    async (testName, startDate, endDate, totalDeleted) => {
-      const stockName = 'StockToBeDeleted';
-      const deleteResponse = await request(app.getHttpServer())
-        .delete(`/stocks/${stockName}`)
-        .query(
-          endDate
-            ? { start_date: startDate, end_date: endDate }
-            : { start_date: startDate },
-        );
-      const content = deleteResponse.body;
+    test.each(deleteStockRecordCases)(
+      'Deleting stock records %s',
+      async (testName, startDate, endDate, totalDeleted) => {
+        const stockName = 'StockToBeDeleted';
+        const deleteResponse = await request(app.getHttpServer())
+          .delete(`/stocks/${stockName}`)
+          .query(
+            endDate
+              ? { start_date: startDate, end_date: endDate }
+              : { start_date: startDate },
+          );
+        const content = deleteResponse.body;
 
-      expect(deleteResponse.status).toEqual(200);
-      expect(content.count).toEqual(totalDeleted);
-    },
-  );
+        expect(deleteResponse.status).toEqual(200);
+        expect(content.count).toEqual(totalDeleted);
+      },
+    );
 
-  test.each(deleteStockRecordCases)(
-    'Deleting stock records that do not exists %s',
-    async (testName, startDate, endDate) => {
-      const stockName = 'StockThatDoNotExists';
-      const deleteResponse = await request(app.getHttpServer())
-        .delete(`/stocks/${stockName}`)
-        .query(
-          endDate
-            ? { start_date: startDate, end_date: endDate }
-            : { start_date: startDate },
-        );
-      const error = deleteResponse.body;
-      expect(error.status).toEqual(404);
-      expect(error.name).toEqual('EST002');
-    },
-  );
+    test.each(deleteStockRecordCases)(
+      'Deleting stock records that do not exists %s',
+      async (testName, startDate, endDate) => {
+        const stockName = 'StockThatDoNotExists';
+        const deleteResponse = await request(app.getHttpServer())
+          .delete(`/stocks/${stockName}`)
+          .query(
+            endDate
+              ? { start_date: startDate, end_date: endDate }
+              : { start_date: startDate },
+          );
+        const error = deleteResponse.body;
+        expect(error.status).toEqual(404);
+        expect(error.name).toEqual('EST002');
+      },
+    );
+  });
 });
